dae9747e38cfb591e5e16a50a14c574a21630855
Pipencho <feliperv87@gmail.com>
2021-08-06 18:43:56 -0500

    Initial commit

---
+++ mobile/src/main/java/com/realitylabs/android/wearable/app/tenniswatchface/MainActivity.java
@@ -0,0 +1,14 @@
+package com.realitylabs.android.wearable.app.tenniswatchface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
